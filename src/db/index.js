042cbe4c6ca565677cacfb6e56750bf70a68d635
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-09-08 16:32:43 +0530

    index.js running

---
+++ src/db/index.js
@@ -0,0 +1,10 @@
+import mongoose, { Mongoose } from "mongoose";
+import { DB_NAME } from "./constants";
+
+const connectDB = async () => {
+    try {
+        
+    } catch (error) {
+        console.log ("MONGOOSEDB connection error")
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 
+
+
+
+
+
+/*
 import express from "express"
 const app = express()
 
@@ -11,12 +17,17 @@ const app = express()
     try {
          await  mongoose.connect(`${process.env.MONGODB_URL}/$ 
             { DB_NAME }`)
-             app.on("error", () => {
-                console.log("ERRR: ",error)
+             app.on("error", (error) => {
+                console.log("ERRR: ",error);
+                throw error
              })
+
+             app.listen(process.env.PORT, () => {
+                console.log(`App is listening on port $ 
+                    {process.env.PORT}`);})
         
     } catch (error) {
         console.error("ERROR : ", error)
         throw err
     }
-})()
+})()*/
