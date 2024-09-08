bd05a94792b18fcb7819e8cb25449423419488e6
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-09-08 23:37:57 +0530

    indx fil running

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config({path: './env'})
 
+import dotenv from "dotenv"
+
+import connectDB from "./src/db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
 
 
 
@@ -15,7 +25,7 @@ const app = express()
 ( async () => {
 
     try {
-         await  mongoose.connect(`${process.env.MONGODB_URL}/$ 
+         await  mongoose.connect(`${process.env.MONGODB_URI}/$ 
             { DB_NAME }`)
              app.on("error", (error) => {
                 console.log("ERRR: ",error);
--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,20 @@
-import mongoose, { Mongoose } from "mongoose";
-import { DB_NAME } from "./constants";
+import 'dotenv/config'; // Load environment variables
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
 
 const connectDB = async () => {
     try {
-        
+        console.log('MONGODB_URI from .env:', process.env.MONGODB_URI);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log ("MONGOOSEDB connection error")
+        console.error("MONGOOSEDB connection error", error);
+        process.exit(1);
     }
 }
+
+export default connectDB;
