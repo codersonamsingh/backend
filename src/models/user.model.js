037388347e375ae4750da19e8409d597ebb98872
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-09-12 22:56:21 +0530

    model running

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,14 +19,40 @@ const userSchema = new Schema ({
        
      },
 
-     email: {
+     fullname: {
       type: String,
       required: true,
       trim: true,
       index: true,
    },
+   avatar: {
+      type: String, // cloudinary url
+      required: true,
+   },
+   coverImage: {
+      type: String, // cloudinary url
+   
+   },
+   watchHistory: [
+      {
+         type: Schema.Types.ObjectId,
+         ref: "Video"
+      }
+   ],
+   password: {
+      type: String,
+      required: [true, 'Password is required']
+   },
+   refreshToken: {
+      type: String
+   }
+   
 
-})
+},
+{
+   timestamps: true,
+}
+)
 
 
 export const User = mongoose.model("User", userSchema)
